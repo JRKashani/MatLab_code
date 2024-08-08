d1819eaf7f0a3f81bcd40899e30f44239ad9c491
JRKashani <72232619+JRKashani@users.noreply.github.com>
2024-08-08 23:41:15 +0300

    final
    after adjusting everything

--- Lab9_10_Q2_code.m
+++ Lab9_10_Q2_code.m
@@ -3,45 +3,48 @@ function neighbouring_pixels()
 %matrix, initialize a random-number matrix of this size, calculate
 %each occurance of a certain range of elements and print the results
 
-    exp_input = input("Enter the numer of rows, an integer between 5 and 10: ");
+    exp_input = input("Enter the number of rows, an integer between 5 and 10: ");
     num_of_rows = valid_input(exp_input);
     
-    exp_input = input("Enter the numer of coloumns, an integer between 5 and 10: ");
+    exp_input = input("Enter the number of coloumns, an integer between 5 and 10: ");
     num_of_cols = valid_input(exp_input);
     
-    pixles_value = 5*rand(num_of_rows, num_of_cols);
+    pixels_value = 5*rand(num_of_rows, num_of_cols);
     steel = [0, 1.5];
     copper = [1.5, 3.4];
     iron = [3.4, 4.1];
     carbon = [4.1, 5];
 
-    pixles_value(pixles_value < steel(2)) = 1;
-    pixles_value(pixles_value < copper(2) & (pixles_value > copper(1))) = 2;
-    pixles_value(pixles_value < iron(2) & (pixles_value > iron(1))) = 3;
-    pixles_value(pixles_value < carbon(2) & (pixles_value > carbon(1))) = 4;
+    pixels_value(pixels_value < steel(2) & (pixels_value >= steel(1)))= 1;
+    pixels_value(pixels_value < copper(2) & (pixels_value >= copper(1))) = 2;
+    pixels_value(pixels_value < iron(2) & (pixels_value >= iron(1))) = 3;
+    pixels_value(pixels_value < carbon(2) & (pixels_value >= carbon(1))) = 4;
     total_amount = zeros(1,4);
+    metal_names = ["steel", "copper", "iron", "carbon"];
 
     for row = 2:num_of_rows-1
         for col = 2:num_of_cols-1
-            if similar_neighbours(pixles_value(row-1:row+1, col-1:col+1), 1)
-                pixles_value(row-1:row+1col-1:col+1) = ...
-                    pixles_value(row-1:row+1col-1:col+1) + 1;
+            if similar_neighbours(pixels_value(row-1:row+1, col-1:col+1), 1)
+                total_amount(pixels_value(row,col)) = ...
+                    total_amount(pixels_value(row,col)) + 1;
             end
         end
     end
+    for metal = 1:length(total_amount)
+        fprintf("The amount of %s is %d.\n", metal_names(metal),...
+            total_amount(metal));
+    end
+end
 
-    
-
-
-function verefied_input = valid_input(sus_input) %a funtion to reduce wasteage
-%of code
+function verified_input = valid_input(sus_input) %a funtion to reduce
+%wasteage of code
      invalid_input = true;
      while invalid_input
          if ~isnumeric(sus_input)
              sus_input = input("It seems not to be a numeric value at all! Try again: ");
          elseif ~isscalar(sus_input)
              sus_input = input("It seems not to be a scalar value. Try again: ");
-         elseif mod(sus_input,1) == 0
+         elseif fix(sus_input) ~= sus_input
              sus_input = input("It seems not to be a positive integer. Try again: ");
          elseif sus_input > 10
               sus_input = input("It seems to be to large of a number. Try again. not smaller than 5, not larger than 10: ");
@@ -51,25 +54,5 @@ function verefied_input = valid_input(sus_input) %a funtion to reduce wasteage
              invalid_input = false;
          end
      end
-     verefied_input = sus_input;
+     verified_input = sus_input;
 end
-
-function does_have_a_neighbour = similar_neighbours(mat, no_diagonals)
-    
-    flex_mat = mat;
-    [num_of_rows, num_of_cols] = size(flex_mat);
-
-    if num_rows ~= num_of_cols || ~rem(num_of_rows, 2)
-        disp("Wrong matrix, bro, needs to have a center.");
-        return;
-    end
-
-    if no_diagonals
-        for row = 1:num_of_rows
-            for col = 1:num_of_cols
-                if col == row || col == num_of_rows - row + 1
-                    flex_mat(row, col) = -inf;
-                end
-            end
-        end
-        flex_mat()
