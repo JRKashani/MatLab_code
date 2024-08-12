81011c158c15d58e6e90734fb42f1607428dd6e4
JRKashani <72232619+JRKashani@users.noreply.github.com>
2024-08-13 02:29:47 +0300

    finishing the code
    and moving to loops instead of vector operations

--- Lab9_10_Q1_code.m
+++ Lab9_10_Q1_code.m
@@ -1,27 +1,45 @@
 function zeros_above %the function gets a matrix of numbers from the user,
-%and rearrnge each coloumn so the zeros will be the leading elements.
+%and rearrnge each column so the zeros will be the leading elements.
+
+%%clc; clear;
 
 %Accept a matrix from the user
 inMAT = input("Enter a matrix of numbers, please: ");
 
-%test the matrix
-while isequal(inMAT, NaN)
-    inMAT = input('It seems there is a "NaN" or "inf" in your input. Can you change it?');
-end
+%testing the matrix
+inappropriate_input = true;
+while inappropriate_input
 
+    %test if the input is a matrix at all
+    if ~ismatrix(inMAT) || ~isnumeric(inMAT)
+        inMAT = input("It seems your input isn't a numeric matrix. Can you change it, please? ");
+        continue; %start the test while again
+    end
+    
+    %test for NaN and inf value
+    if any(isnan(inMAT(:))) || any(isinf(inMAT(:)))
+        inMAT = input('It seems there is a "NaN" or "inf" in your input. Can you change it?');
+        continue; %start the test while again
+    end
+    %if the two conditions failed, the input is valid
+    inappropriate_input = false;   
+end  
+
+    
 %initalizing the output matrix and getting its size
-[numRows, numCols] = size(inMAT);
-outMAT = zeros([numRows, numCols]);
+    [numRows, numCols] = size(inMAT);
+    outMAT = zeros([numRows, numCols]);
 
-%Moving through each coloumn
+
+%Moving through each column
 for colIdx = 1:numCols
     col = inMAT(:,colIdx); 
-    nonZeroCol = col(col ~= 0); %removing the zeroes from the coloumn
+    nonZeroCol = col(col ~= 0); %removing the zeroes from the column
     numNonZeros = length(nonZeroCol);
     index_range = (numRows - numNonZeros + 1 : numRows); % calculating...
     %...the relevant index range for the replacement
     outMAT(index_range, colIdx) = nonZeroCol; %replace...
-    %...the corresponding zeroes matrix with the non-zeroes coloumn
+    %...the corresponding zeroes matrix with the non-zeroes column
 end
 
 % Displaying the results
--- Lab9_10_Q2_code.m
+++ Lab9_10_Q2_code.m
@@ -2,26 +2,48 @@ function neighbouring_pixels()
 %The function will recieve from the user the dimensions of a...
 %matrix, initialize a random-number matrix of this size, calculate
 %each occurance of a certain range of elements and print the results
-
+    
+%clc; clear;
+    %recieving input from the user and testing it
     exp_input = input("Enter the number of rows, an integer between 5 and 10: ");
     num_of_rows = valid_input(exp_input);
     
     exp_input = input("Enter the number of coloumns, an integer between 5 and 10: ");
     num_of_cols = valid_input(exp_input);
     
-    pixels_value = 5*rand(num_of_rows, num_of_cols);
+    %initiallizing the variables and vectors I will use
+    pixels_value_raw = 5*rand(num_of_rows, num_of_cols);
+    pixels_value = zeros(num_of_rows, num_of_cols);
     steel = [0, 1.5];
     copper = [1.5, 3.4];
     iron = [3.4, 4.1];
     carbon = [4.1, 5];
-
-    pixels_value(pixels_value < steel(2) & (pixels_value >= steel(1)))= 1;
-    pixels_value(pixels_value < copper(2) & (pixels_value >= copper(1))) = 2;
-    pixels_value(pixels_value < iron(2) & (pixels_value >= iron(1))) = 3;
-    pixels_value(pixels_value < carbon(2) & (pixels_value >= carbon(1))) = 4;
     total_amount = zeros(1,4);
     metal_names = ["steel", "copper", "iron", "carbon"];
+    
+    %making a range of values into a single one, for 4 different ranges
+    for row = 1:num_of_rows
+        for col = 1:num_of_cols
+            temp = pixels_value_raw(row, col);
+            if (temp < steel(2) && (temp >= steel(1)))
+                pixels_value(row, col) = 1;
+            elseif (temp < copper(2) && (temp >= copper(1)))
+                pixels_value(row, col) = 2;
+            elseif (temp < iron(2) && (temp >= iron(1)))
+                pixels_value(row, col) = 3;
+            elseif (temp < carbon(2) && (temp >= carbon(1)))
+                pixels_value(row, col) = 4;
+            end
+        end
+    end
 
+    %pixels_value(pixels_value < steel(2) & (pixels_value >= steel(1)))= 1;
+    %pixels_value(pixels_value < copper(2) & (pixels_value >= copper(1))) = 2;
+    %pixels_value(pixels_value < iron(2) & (pixels_value >= iron(1))) = 3;
+    %pixels_value(pixels_value < carbon(2) & (pixels_value >= carbon(1))) = 4;
+    
+    %testing each pixle in the center biggest square for similar neighbors
+    %and summing up the results
     for row = 2:num_of_rows-1
         for col = 2:num_of_cols-1
             if similar_neighbours(pixels_value(row-1:row+1, col-1:col+1), 1)
@@ -30,9 +52,15 @@ function neighbouring_pixels()
             end
         end
     end
+
+    %printing the results
+    fprintf("This is the original matrix: \n");
+    disp(pixels_value_raw);
+    fprintf("This is the matrix after the rounding: \n");
+    disp(pixels_value);
     for metal = 1:length(total_amount)
-        fprintf("The amount of %s is %d.\n", metal_names(metal),...
-            total_amount(metal));
+        fprintf("The amount of %s (%d) is %d.\n", metal_names(metal),...
+            metal, total_amount(metal));
     end
 end
 
